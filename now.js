648ee92457e7c33434684e7a0db47f32204453d5
Pelagia Sykoudi <pelagia.sykoudi@philips.com>
2019-04-13 15:18:42 +0200

    initial set up

---
+++ now.js
@@ -0,0 +1,7 @@
+{
+  "version": 2,
+  "name": "workshop-1",
+  "builds": [{ "src": "./package.json", "use": "@now/next" }],
+  "routes": [{ "src": "/(.*)", "dest": "./src" }],
+  "alias": "workshop-1"
+}
