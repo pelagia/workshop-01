5cbcd7ef7231d768294293f7f2dc2fe99a717fa4
Pelagia Sykoudi <pelagia.sykoudi@philips.com>
2019-04-13 15:35:21 +0200

    fix deployment file

--- now.js
+++
@@ -1,7 +0,0 @@
-{
-  "version": "2",
-  "name": "workshop-1",
-  "builds": [{ "src": "./package.json", "use": "@now/next" }],
-  "routes": [{ "src": "/(.*)", "dest": "./src" }],
-  "alias": "workshop-1"
-}
